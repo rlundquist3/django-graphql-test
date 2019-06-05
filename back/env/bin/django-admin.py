ac8ce917d18a486c8c20eeb394ed74ae647198b6
Riley Lundquist <rlundquist3@gmail.com>
2019-06-04 22:53:23 -0700

    got initial api running

---
+++ back/env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/riley/django-test/django-react-graphql-simplenote/back/env/bin/python3.7
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
