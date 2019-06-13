828f46612bd48d46ea82d45d2b6405656a6f6477
Riley Lundquist <rlundquist3@gmail.com>
2019-06-12 20:25:10 -0700

    set up apollo client - kinda half-assed, but gonna use MobX and provide it in app state

--- front/src/index.jsx
+++ front/src/index.jsx
@@ -2,15 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './components/header';
 
-const Hello = ({ name }) => (
-  <div>
-    <Header />
+import { ApolloProvider } from 'react-apollo';
+import ApolloClient from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+const httpLink = createHttpLink({
+  uri: '<http://localhost:8000/graphql/>',
+});
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink,
+});
+
+const Hello = ({ name, ...props }) => {
+  console.log(props)
+  return (
     <div>
-      <h1>Hello {name}</h1>
+      <Header />
+      <div>
+        <h1>Hello {name}</h1>
+      </div>
     </div>
-  </div>
+  )
+};
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Hello name='Freja' client={client} />
+  </ApolloProvider>,
+  document.getElementById('app')
 );
-
-let App = document.getElementById('app');
-
-ReactDOM.render(<Hello name='Freja' />, App);
